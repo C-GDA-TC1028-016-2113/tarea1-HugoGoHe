580706579e44ef73c836492b8d0481cb0c1ca378
HugoGoHe <A01640856@itesm.mx>
2021-08-18 00:15:08 +0000

    envío 1

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,6 +1,10 @@
 def main():
     #escribe tu código abajo de esta línea
-    pass
+    calificaciones = []
+    for i in range(0,4):
+        calificaciones.append(float(input("Calificación de la materia: ")))
+    promedio = sum(calificaciones)/4
+    print("El promedio es: " + str(promedio))
 
 if __name__ == '__main__':
     main()
